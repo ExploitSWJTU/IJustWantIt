4a43ea26d46cfae4db56dfb8281d580ae95d27e5
mechtifs <mechtifs@gmail.com>
2023-03-07 20:22:30 +0800

    Add jwc add_course

--- jwc.py
+++ jwc.py
@@ -7,16 +7,14 @@ import muggle_ocr
 course_list_re = re.compile(r'<tr>[\s\S]+?<\/tr>')
 course_url_re = re.compile(r'window\.location=\'\.\.(.+?)\'')
 confirm_url_re = re.compile(r'editStudentCourseSysListConfirm\(.+?\'([0-9A-F]{16})\',\'(.+?)\',\'(.+?)\'\)')
+teach_id_re = re.compile(r'<span id="teachIdChoose.+?" style="display:none;">([0-9A-F]{16})<\/span>')
 
-# For change course only
 class JWC:
-    def __init__(self, username, password, query, target):
+    def __init__(self, username, password):
         self.s = requests.Session()
         self.base_url = 'http://jwc.swjtu.edu.cn'
         self.username = username
         self.password = password
-        self.query = query
-        self.target = target
 
     def login(self):
         sdk = muggle_ocr.SDK(model_type=muggle_ocr.ModelType.Captcha)
@@ -36,30 +34,52 @@ class JWC:
 
         self.s.post(self.base_url+'/vatuu/UserLoadingAction')
 
-    def get_course_list(self):
+    def query_add_course(self, course_id):
+        data = {
+            'setAction': 'studentCourseSysSchedule',
+            'selectAction': 'TeachID',
+            'key1': course_id,
+        }
+        r = self.s.post(self.base_url+'/vatuu/CourseStudentAction', data=data)
+        try:
+            return teach_id_re.search(r.text).group(1)
+        except AttributeError:
+            return ''
+
+    def add_course(self, course_id):
+        teach_id = self.query_add_course(course_id)
+        if not teach_id:
+            print('Not available!')
+            return
+        r = self.s.get(self.base_url+'/vatuu/CourseStudentAction?setAction=addStudentCourseApply&teachId={}&isBook=0&tt={}'.format(teach_id, int(time.time()*1000)))
+        print(r.text)
+
+    def get_change_course_list(self):
         r = self.s.get(self.base_url+'/vatuu/CourseStudentAction?setAction=studentCourseSysList&viewType=editCourse')
         return r.text
 
-    def query_course(self):
-        for course in course_list_re.findall(self.get_course_list()):
-            if self.query in course:
+    def query_change_course(self, query, target):
+        for course in course_list_re.findall(self.get_change_course_list()):
+            if query in course:
                 r = self.s.get(self.base_url+course_url_re.search(course).group(1))
                 for course in course_list_re.findall(r.text):
-                    if self.target in course:
+                    if target in course:
                         try:
                             return confirm_url_re.search(course).groups()
                         except AttributeError:
                             return '', '', ''
 
-    def change_course(self):
-        list_id, course_code, teach_id = self.query_course()
+    def change_course(self, query, target):
+        list_id, course_code, teach_id = self.query_change_course(query, target)
         if not list_id:
-            print('Not available')
+            print('Not available!')
             return
         r = self.s.get(self.base_url+'/vatuu/CourseStudentAction?setAction=editStudentCourseList&listId={}&courseCode={}&teachId={}&tt={}'.format(list_id, course_code, teach_id, int(time.time()*1000)))
         print(r.text)
 
+
 if __name__ == '__main__':
-    jwc = JWC('username', 'password', 'query', 'target')
+    jwc = JWC('username', 'password')
     jwc.login()
-    jwc.change_course()
+    jwc.change_course('query', 'target')
+    jwc.add_course('course_id')
